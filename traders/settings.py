a7c1087c2a659e3cc33522007c15b40c05a9851e
Syed Mustafan <mustafanadeem627@gmail.com>
2021-02-05 22:09:30 +0500

    Update Settings

--- traders/settings.py
+++ traders/settings.py
@@ -75,26 +75,26 @@ WSGI_APPLICATION = 'traders.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'traderdb',
-#         'USER': 'postgres',
-#         'PASSWORD': 'eva987987',
-#         'HOST': 'localhost',
-#         'PORT': '5432'
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'traderapi1',
-        'USER': 'root',
-        'PASSWORD': '',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'traderdb',
+        'USER': 'postgres',
+        'PASSWORD': 'eva987987',
         'HOST': 'localhost',
-        'PORT': ''
+        'PORT': '5432'
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'traderapi1',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',
+#         'PORT': ''
+#     }
+# }
 
 
 # Password validation
