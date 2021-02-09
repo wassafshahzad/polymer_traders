117c6d4909ebd409154acc91d61d66c6093a511b
wassaf <wassafshahzad@gmail.com>
2021-02-09 16:11:05 +0500

    added validations

--- tradersapi/services/user_service/serializers.py
+++ tradersapi/services/user_service/serializers.py
@@ -4,6 +4,16 @@ from tradersapi.models import UserProfileModel
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
+
+    def validate(self, attrs):
+        if self._has_user_profile():
+            raise serializers.ValidationError(
+                "Profile for this user already exist")
+        return super().validate(attrs)
+
+    def _has_user_profile(self):
+        return UserProfileModel.objects.filter(owner=self.context.get('user'))
+
     class Meta:
         model = UserProfileModel
         fields = ['name', 'company_name', 'phone_number', 'profile_picture']
--- tradersapi/services/user_service/views.py
+++ tradersapi/services/user_service/views.py
@@ -1,3 +1,4 @@
+from django.http import request
 from rest_framework import generics, permissions
 from tradersapi.models import UserProfileModel
 from .serializers import UserProfileSerializer
@@ -7,3 +8,8 @@ class UserProfileListCreateAPIView(generics.ListCreateAPIView):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
     queryset = UserProfileModel.objects.all()
     serializer_class = UserProfileSerializer
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context["user"] = self.request.user
+        return context
