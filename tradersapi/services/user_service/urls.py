56e7d0e3024a25a5a05256807ffd473c10c8275a
wassaf <wassafshahzad@gmail.com>
2021-02-13 17:00:45 +0500

    added reversable names to urls

--- tradersapi/services/user_service/urls.py
+++ tradersapi/services/user_service/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import UserProfileListCreateAPIView, AuthUserCreateAPIView
 urlpatterns = [
-    path('profiles/', UserProfileListCreateAPIView.as_view()),
-    path('signup/', AuthUserCreateAPIView.as_view())
+    path('profiles/', UserProfileListCreateAPIView.as_view(), name='profile'),
+    path('signup/', AuthUserCreateAPIView.as_view(), name='signup')
 ]
