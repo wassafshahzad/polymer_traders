fdb537c2849564f3efef2f6f2c8e67b17b8af544
wassaf <wassafshahzad@gmail.com>
2021-03-07 18:03:42 +0500

    added user prfle test

--- tradersapi/tests/userprofile_test.py
+++ tradersapi/tests/userprofile_test.py
@@ -42,3 +42,52 @@ class UserProfileListCreateTestCase(APITestCase):
         }
         response = self.client.post(reverse('profile'), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+class UserProductTestCreateRetriveCases(APITestCase):
+    fixtures = ['data.json']
+    data = {
+        "quantity": "10 drum",
+        "price_per_bad": "10",
+        "chemical": 2,
+        "chemical_type": 1
+    }
+
+    def test_create_user_product_fail_401(self):
+        response = self.client.post(reverse('get_product'), self.data)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_user_product_fail_400_chemical_not_exist(self):
+        data = {
+            "quantity": "10 drum",
+            "price_per_bad": "10",
+            "chemical": 100,
+            "chemical_type": 1
+        }
+        user = User.objects.get(username="Test2")
+        self.client.force_authenticate(user=user)
+        response = self.client.post(reverse('get_product'), data)
+        self.assertEqual(response.data['chemical'][0].code, 'does_not_exist')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_user_product_fail_400_chemical_not_exist(self):
+        data = {
+            "quantity": "10 drum",
+            "price_per_bad": "10",
+            "chemical": 1,
+            "chemical_type": 100
+        }
+        user = User.objects.get(username="Test2")
+        self.client.force_authenticate(user=user)
+        response = self.client.post(reverse('get_product'), data)
+        self.assertEqual(
+            response.data['chemical_type'][0].code, 'does_not_exist')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_user_product_fail_400_profle_not_exist(self):
+        user = User.objects.get(username="Test2")
+        self.client.force_authenticate(user=user)
+        response = self.client.post(reverse('get_product'), self.data)
+        self.assertEqual(
+            response.data['non_field_errors'][0].code, 'invalid')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
