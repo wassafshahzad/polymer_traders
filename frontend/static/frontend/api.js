017ca56c1f0f5810e37fef51e7c17670af58274f
WassafShahzad <wassafshahzad@A006-00472.local>
2021-07-09 18:32:54 +0500

    updated api.js to attach module function to window

--- frontend/static/frontend/api.js
+++ frontend/static/frontend/api.js
@@ -2,9 +2,8 @@ import {BASE_URL,USER_SERVICE,SLASH} from './constants.js'
 import customFetch  from './network.js'
 
 
-const authService  = {
+window.authService  = {
     signUpUser  : (data) => customFetch(BASE_URL + USER_SERVICE + 'signup/','POST',data)
 }
 
 
-export {authService}
