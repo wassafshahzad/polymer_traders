b2b0c0435308e64e94f3ae7e53727c95b48f25cb
WassafShahzad <wassafshahzad@A006-00472.local>
2021-07-11 15:45:30 +0500

    updated error handles

--- frontend/static/frontend/network.js
+++ frontend/static/frontend/network.js
@@ -1,50 +1,52 @@
-const ERROR_CODES = {
-    404: 'Content not available: Sorry, this content must have been changed or deleted.',
-    403: 'Unauthorized: Sorry, You do not have permission to access this content',
-    400: 'Bad request: Sorry the request must be malformed',
-    409: 'Sorry, this action is not possible at the moment.',
-    405: 'Sorry, this action is not possible at the moment.',
-};
-
-
-  function handleErrors(error) {
-    if (
-      error.message === 'Network Error' ||
-      error.message === 'Failed to fetch'
-    ) {
-      alert(
-        'Request could not be completed, please check your internet connection.'
-      );
-    } else if (error.response && error.response.data.error) {
-      alert(error.response.data.error);
-    } else if (error.response && error.response.data.message) {
-      alert(error.response.data.message);
-    } else if (error.response && error.response.status === 400) {
-      alert(error.response.data);
-    } else if (error.response && error.response.status) {
-      alert(ERROR_CODES[error.response.status]);
-    } else {
-      alert('Sorry, something went wrong.');
-    }
+import { ERROR_CODES } from "./constants.js";
+class CustomError extends Error{
+  constructor(status,message){
+      super(message)
+      this.status = status
   }
+} 
 
-
-function customFetch(url,method, data=undefined, is_auth = true, content = 'application/json'){
-    return fetch(url, {
-        method: method,
-        headers: {
-            Authorization: is_auth ? `Token ${localStorage.getItem('key')}` : null, 
-            'Content-Type': content
-        },
-        body: data
-    }).then(response =>{
-        if (!response.ok) {
-            throw new Error(response)
-        }
-        return  response.json()
-    }).catch(error => {
-        handleErrors(error)
-    })
+function _handleErrors({ status, data, message }) {
+  if (
+    message === 'Network Error' ||
+    message === 'Failed to fetch'
+  ) {
+    alert(
+      'Request could not be completed, please check your internet connection.'
+    );
+  }
+  if (status) {
+    alert(ERROR_CODES[status]);
+  } else {
+    alert("Sorry, something went wrong.");
+  }
 }
 
-export default customFetch
+
+function customFetch(
+  url,
+  method,
+  data = undefined,
+  is_auth = true,
+  content = "application/json"
+) {
+  return fetch(url, {
+    method: method,
+    headers: {
+      Authorization: is_auth ? `Token ${localStorage.getItem("key")}` : null,
+      "Content-Type": content,
+    },
+    body: data,
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new CustomError(response.status,response.statusText);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      _handleErrors(error);
+    });
+}
+
+export default customFetch;
