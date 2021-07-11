3484a488874db5a57e03ddbbad46b9f54d4fd678
WassafShahzad <wassafshahzad@A006-00472.local>
2021-07-11 15:43:59 +0500

    moved error to contants

--- frontend/static/frontend/constants.js
+++ frontend/static/frontend/constants.js
@@ -1,7 +1,13 @@
 const BASE_URL = '/trader/api/v1/'
 const USER_SERVICE = 'user_service'
 const SLASH = '/'
+const ERROR_CODES = {
+    404: 'Content not available: Sorry, this content must have been changed or deleted.',
+    403: 'Unauthorized: Sorry, You do not have permission to access this content',
+    400: 'Bad request: Sorry the request must be malformed',
+    409: 'Sorry, this action is not possible at the moment.',
+    405: 'Sorry, this action is not possible at the moment.',
+};
 
 
-
-export {BASE_URL,USER_SERVICE,SLASH}
+export {BASE_URL,USER_SERVICE,SLASH,ERROR_CODES}
