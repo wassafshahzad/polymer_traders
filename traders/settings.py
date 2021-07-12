a3178ce4f2654cf757244e42060076b48d481616
wassaf <wassafshahzad@gmail.com>
2021-07-13 00:18:14 +0500

    updated setting to run bth locally and remotlly

--- traders/settings.py
+++ traders/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -138,5 +140,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STAGING = os.environ.get('STAGING', False)
 STATIC_URL = '/static/'
-STATIC_ROOT = Path.joinpath(BASE_DIR, 'static')
+
+if STAGING:
+    import django_on_heroku
+    STATIC_ROOT = Path.joinpath(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    django_on_heroku.settings(locals())
+
+else:
+    STATIC_ROOT = Path.joinpath(BASE_DIR, 'static')
