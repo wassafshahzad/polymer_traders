d428e63a601bfa64d7948a3cdafebe9c2fa4e1c8
WassafShahzad <wassafshahzad@A006-00472.local>
2021-07-14 12:30:58 +0500

    on signUP redirect

--- frontend/views.py
+++ frontend/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 
 
 def sign_up(request):
     return render(request, 'frontend/SignUpPage.html')
+
+
+def welcome_page(request):
+    if not request.user:
+        return redirect('signup')
+    else:
+        return render(request, 'frontend/Welcome.html', {
+            'user_name', request.user.user_name})
