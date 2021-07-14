b3e71a11e1579308206c3c533d0222f3a1d16b11
WassafShahzad <wassafshahzad@A006-00472.local>
2021-07-14 10:45:50 +0500

    added smtp server config

--- traders/settings.py
+++ traders/settings.py
@@ -151,3 +151,17 @@ if STAGING:
 
 else:
     STATIC_ROOT = Path.joinpath(BASE_DIR, 'static')
+
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'postmaster@sandbox41359da0f4f64d7bb28d4a9e2f945aa3.mailgun.org'
+EMAIL_HOST_PASSWORD = os.environ.get('SMTP_PASSWORD')
