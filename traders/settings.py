a4109d65da900fd41fcb04fbda707b6ed6296446
WassafShahzad <wassafshahzad@A006-00472.local>
2021-07-14 10:57:56 +0500

    configured smtp server settings

--- traders/settings.py
+++ traders/settings.py
@@ -161,7 +161,7 @@ CELERY_TIMEZONE = 'UTC'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_HOST = os.environ.get('SMTP_HOST')
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'postmaster@sandbox41359da0f4f64d7bb28d4a9e2f945aa3.mailgun.org'
+EMAIL_HOST_USER = os.environ.get('SMTP_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('SMTP_PASSWORD')
