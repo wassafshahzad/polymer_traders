11cf1965034dc7dc832e4d5273cb3c6532af80f8
WassafShahzad <wassafshahzad@A006-00472.local>
2021-07-15 17:04:28 +0500

    formtat fixes

--- tradersapi/services/user_service/views.py
+++ tradersapi/services/user_service/views.py
@@ -5,8 +5,7 @@ from rest_framework.response import Response
 
 from tradersapi.models import UserProfileModel, UserPost
 from tradersapi.util.permissions import IsOwnerOrReadOnly
-from .serializers import UserProfileSerializer, \
-    AuthUserSerializer, UserPostSerializer
+from .serializers import UserProfileSerializer, AuthUserSerializer, UserPostSerializer
 
 
 class UserProfileListCreateAPIView(generics.ListCreateAPIView):
@@ -20,8 +19,12 @@ class UserProfileListCreateAPIView(generics.ListCreateAPIView):
         return context
 
     def list(self, request, *args, **kwargs):
-        if kwargs.get('current', False):
-            return Response(self.get_serializer(UserProfileModel.objects.get(owner=request.user)).data)
+        if kwargs.get("current", False):
+            return Response(
+                self.get_serializer(
+                    UserProfileModel.objects.get(owner=request.user)
+                ).data
+            )
         return super().list(request, *args, **kwargs)
 
 
@@ -32,10 +35,10 @@ class AuthUserCreateAPIView(generics.CreateAPIView):
 
 class UserPostListCreateAPIView(generics.ListCreateAPIView):
     serializer_class = UserPostSerializer
-    queryset = UserPost.objects.filter(status='1').order_by('-created_at')
+    queryset = UserPost.objects.filter(status="1").order_by("-created_at")
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['created_by', 'post_type', 'chemical', 'chemical_type']
+    filterset_fields = ["created_by", "post_type", "chemical", "chemical_type"]
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
@@ -45,15 +48,14 @@ class UserPostListCreateAPIView(generics.ListCreateAPIView):
 
 class UserPostRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = UserPostSerializer
-    queryset = UserPost.objects.filter(status='1')
+    queryset = UserPost.objects.filter(status="1")
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 
 class UserProfileRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
     serializer_class = UserProfileSerializer
     queryset = UserProfileModel.objects.all()
-    permission_classes = [
-        permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
 
     def get_serializer_context(self):
         context = super().get_serializer_context()
