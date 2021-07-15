1de98872148899d2d04ba4d91f107df51d08b276
WassafShahzad <wassafshahzad@A006-00472.local>
2021-07-15 18:28:23 +0500

    updated enum

--- tradersapi/task.py
+++ tradersapi/task.py
@@ -8,9 +8,9 @@ from tradersapi.util import EmailConstants
 def send_welcome_email_on_signup(username, email):
     message = get_template("tradersapi/email.html").render({"username": username})
     mail = EmailMessage(
-        subject=EmailConstants.WELCOME_SUBJECT,
+        subject=EmailConstants.WELCOME_SUBJECT.value,
         body=message,
         to=[email],
     )
-    mail.content_subtype = EmailConstants.HTML_TYPE
+    mail.content_subtype = EmailConstants.HTML_TYPE.value
     return mail.send()
--- tradersapi/util/constants.py
+++ tradersapi/util/constants.py
@@ -4,6 +4,3 @@ from enum import Enum
 class EmailConstants(Enum):
     WELCOME_SUBJECT = "Welcome Message"
     HTML_TYPE = "html"
-
-    def __repr__(self) -> str:
-        return self.value
